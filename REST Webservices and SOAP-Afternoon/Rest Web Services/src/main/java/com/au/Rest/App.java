a86ba7da6d9777e7c73fc732807e8a23ddf86f91
kaushikkandikonda <78872313+kaushikkandikonda@users.noreply.github.com>
2021-03-10 00:04:18 +0530

    Add files via upload

---
+++ REST Webservices and SOAP-Afternoon/Rest Web Services/src/main/java/com/au/Rest/App.java
@@ -0,0 +1,11 @@
+package com.au.Rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!REST API");
+    }
+}
---
+++ REST Webservices and SOAP-Afternoon/Rest Web Services/src/main/java/com/au/Rest/Controller.java
@@ -0,0 +1,30 @@
+package com.au.Rest;
+
+import javax.websocket.server.PathParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+
+@Path("/rest")
+public class Controller {
+
+	@Path("/")
+	public String function() {
+
+		return "Rest Web Service Session";
+
+	}
+
+	@Produces(MediaType.TEXT_PLAIN)
+	@GET
+	@Path("/{name}")
+	public String function(@Context HttpHeaders header, @PathParam("name") String name) {
+
+		return "Hello" + name;
+
+	}
+
+}
